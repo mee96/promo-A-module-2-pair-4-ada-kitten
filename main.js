62f0c0afcbeccc5c704eb6f64ba42c9542b2030c
Juande <juande@Juandes-MacBook-Pro-2.local>
2024-10-17 11:00:19 +0200

    Kitten const and uppercase

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
 "user strict";
 
 console.log("konichiwa, sekai");
-const kittenDesc1 = "Porte elegante, su patrón de color tan característico y sus ojosde un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente."
+const kittenDesc1 =
+  "Porte elegante, su patrón de color tan característico y sus ojosde un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
 
-const kittenDesc2 ="Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo."
+const kittenDesc2 =
+  "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
 
-const kittenDesc3 = "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta."
+const kittenDesc3 =
+  "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
 
+const Gato1 = document.querySelector(".neko1");
+const Gato2 = document.querySelector(".neko2");
+const Gato3 = document.querySelector(".neko3");
+
+const kittenImg1 = "https://dev.adalab.es/gato-siames.webp";
+const kittenImg2 = "https://dev.adalab.es/sphynx-gato.webp";
+const kittenImg3 = "https://dev.adalab.es/maine-coon-cat.webp";
+
+const kittenName1 = "Anastasio".toUpperCase();
+const kittenName2 = "Fiona".toUpperCase();
+const kittenName3 = "Cielo".toUpperCase();
+
+const kittenRace1 = "Siamés";
+const kittenRace2 = "Sphynx";
+const kittenRace3 = "Maine Coon";
 
 const nekoList = document.querySelector(".js-list");
 nekoList.innerHTML += `<li class="card neko1">
             <article>
               <img
                 class="card_img"
-                src="https://dev.adalab.es/gato-siames.webp"
+                src= ${kittenImg1}
                 alt="siames-cat"
               />
-              <h3 class="card_title">Anastacio</h3>
-              <h4 class="card_race">Siamés</h4>
+              <h3 class="card_title">${kittenName1}</h3>
+              <h4 class="card_race">${kittenRace1}</h4>
               <p class="card_description kittenDesc1">
                 ${kittenDesc1}
               </p>
@@ -27,11 +45,11 @@ nekoList.innerHTML += `<li class="card neko1">
 nekoList.innerHTML += ` <li class="card neko2">
             <img
               class="card_img"
-              src="https://dev.adalab.es/sphynx-gato.webp"
+              src= ${kittenImg2}
               alt="sphynx-cat"
             />
-            <h3 class="card_title">Fiona</h3>
-            <h4 class="card_race">Sphynx</h4>
+            <h3 class="card_title">${kittenName2}</h3>
+            <h4 class="card_race">${kittenRace2}</h4>
             <p class="card_description kittenDesc2">
               ${kittenDesc2}
             </p>
@@ -40,11 +58,11 @@ nekoList.innerHTML += ` <li class="card neko2">
 nekoList.innerHTML += `<li class="card neko3">
             <img
               class="card_img"
-              src="https://dev.adalab.es/maine-coon-cat.webp"
+              src=${kittenImg3}
               alt="maine-coon-cat"
             />
-            <h3 class="card_title">Cielo</h3>
-            <h4 class="card_race">Maine Coon</h4>
+            <h3 class="card_title">${kittenName3}</h3>
+            <h4 class="card_race">${kittenRace3}</h4>
             <p class="card_description kittenDesc3">
               ${kittenDesc3}
             </p>
@@ -63,13 +81,8 @@ cancelButton.addEventListener("click", (Event) => {
   form.classList.add("collapsed");
 });
 
-
 const searchButton = document.querySelector(".js_button-search");
 const descrInput = document.querySelector(".js_in_search_desc");
-
-const Gato1 = document.querySelector(".neko1")
-const Gato2 = document.querySelector(".neko2")
-const Gato3 = document.querySelector(".neko3")
 
 searchButton.addEventListener("click", (ev) => {
   ev.preventDefault();
@@ -78,16 +91,16 @@ searchButton.addEventListener("click", (ev) => {
   Gato1.style.display = "none";
   Gato2.style.display = "none";
   Gato3.style.display = "none";
-  
+
   if (kittenDesc1.includes(descrSearchText)) {
-  Gato1.style.display = "block"
-}
+    Gato1.style.display = "block";
+  }
 
-if (kittenDesc2.includes(descrSearchText)) {
-   Gato2.style.display = "block"
-}
+  if (kittenDesc2.includes(descrSearchText)) {
+    Gato2.style.display = "block";
+  }
 
-if (kittenDesc3.includes(descrSearchText)) {
-  Gato3.style.display = "block"
-}
+  if (kittenDesc3.includes(descrSearchText)) {
+    Gato3.style.display = "block";
+  }
 });
